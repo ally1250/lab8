5290172228d1e8f1ae987ed1883abf990f7c7a17
Wang, Danling <daw001@ucsd.edu>
2021-02-25 11:05:53 -0800

    finished lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'Op5BosyEDy2W5Lzg1Kza8t3GAjUEsBnn';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.87857434280311, -117.23586454487881],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false,
+	});
+
+	L.marker([32.87857434280311, -117.23586454487881]).addTo(map);
 }
